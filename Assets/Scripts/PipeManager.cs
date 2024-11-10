a7964086e58facf3fa7155cc2a41fedd29fc139e
Rupam Debnath <rdebmail@gmail.com>
2024-11-10 16:15:57 +0100

    Discarded ols models, got new models with respect to scale of each cell that fits perfectly, rotation logic for bend pipes

--- Assets/Scripts/PipeManager.cs
+++ Assets/Scripts/PipeManager.cs
@@ -23,39 +23,54 @@ public class PipeManager : MonoBehaviour
     public int[] cells;
     public int count;
     public Vector3Int currentPoint;
-    public Vector3Int nextPoint;
+    public Vector3Int orgDirection;
+    public Vector3Int nextpoint;
 
     //Check
-    public bool pipingPossible = true;
-
-    private Vector3Int previousDirection = Vector3Int.zero;
+    public bool pipingPossible;
     //Initialization
     public void Initialize()
     {
         //currentPipe = MakeNewPipe(new Vector3Int(lenX/2,lenY/2,lenZ/2));
         //currentPoint = new Vector3Int(Random.Range(0, lenX), Random.Range(0, lenY), Random.Range(0, lenZ));
-        currentPoint = new Vector3Int((int)lenX / 2, (int)lenY / 2, (int)lenZ / 2);
+        //currentPoint = new Vector3Int((int)lenX / 2, (int)lenY / 2, (int)lenZ / 2);
+        CalculateHalfDistance(lenX, lenY, lenZ);
         cells = new int[lenX * lenY * lenZ];
-        pdirection = Vector3Int.zero;
+        //pdirection = Vector3Int.zero;
         pipeItems = new List<GameObject>();
-
+        //nextpoint = SelectPoint(currentPoint);
+        orgDirection = Vector3Int.zero;
     }
 
     //PlacePipe function to be called for instantiating a model
     public void PlacePipe()
     {
-        if (count >= cells.Length)
-            return;
-
-        if (CheckifSpotAvailable(currentPoint))
+/*        if (count >= cells.Length)
+            return;*/
+        nextpoint = SelectPoint(currentPoint);
+        Debug.Log("Next" + (nextpoint));
+        Debug.Log("NextDiff" + (currentPoint - nextpoint));
+        while (nextpoint.x == -1)
         {
-            pipeItems.Add(MakeNewPipe(currentPoint));
-            count++;
+            MakeNewPipe(currentPoint, currentPoint);
+            currentPoint = CheckNewPipeRequirements();
+
+            //break
+            if (currentPoint.x == -1)
+            {
+                pipingPossible = false;
+                break;
+            }
             Set1DPositionValueOccupied(currentPoint);
-            currentPoint = SelectPoint(currentPoint);
+            nextpoint = SelectPoint(currentPoint);
         }
-        else
-            return;
+
+        if (pipingPossible)
+        {
+            MakeNewPipe(currentPoint, nextpoint);
+            currentPoint = nextpoint;
+            Set1DPositionValueOccupied(currentPoint);
+        }
     }
 
     //Check if the spot is empty or full
@@ -67,11 +82,48 @@ public class PipeManager : MonoBehaviour
             return false;
     }
 
-    GameObject MakeNewPipe(Vector3Int npoint)
+    void MakeNewPipe(Vector3Int cpoint, Vector3Int npoint)
     {
-        GameObject newPipe = Instantiate(bendPipe, this.transform);
-        newPipe.transform.position = new Vector3(npoint.x, npoint.y, npoint.z);
-        return newPipe;
+        GameObject pipetype = hollowPipe;
+        Vector3Int nDirection = npoint - cpoint;
+        Vector3 npos = new Vector3(settings * (cpoint.x - halfX), settings * (cpoint.y - halfY), settings * (cpoint.z - halfZ));
+
+        //first and last pipe
+        if (orgDirection.magnitude < 0.1f)
+        {
+            orgDirection = nDirection;
+            MakeBulbPipe(npos, orgDirection);
+            return;
+        }
+        if (nDirection.magnitude < 0.1f)
+        {
+            MakeBulbPipe(npos, -orgDirection);
+            orgDirection = Vector3Int.zero;
+            return;
+        }
+
+        //No change in direction
+        if (nDirection.x * orgDirection.x + nDirection.y * orgDirection.y + nDirection.z * orgDirection.z > 0.9)
+        {
+            pipetype = Instantiate(hollowPipe, npos, Quaternion.FromToRotation(Vector3Int.forward, orgDirection), this.transform);
+            pipetype.transform.localPosition = npos;
+            return;
+        }
+
+        // Direction change, bend Pipe formation
+        Vector3Int pipeDir = nDirection - orgDirection;
+        Quaternion bendPipeRot = Quaternion.LookRotation(pipeDir, Vector3.up);
+        if (pipeDir.y == 0)
+        {
+            bendPipeRot *= Quaternion.AngleAxis(90, Vector3.forward);
+            pipetype = Instantiate(bendPipe, npos, bendPipeRot, this.transform);
+            pipetype.transform.localPosition = npos;
+            //Pipes.Add(newPipe);
+        }
+        orgDirection = nDirection;
+        //GameObject newPipe = Instantiate(pipetype, this.transform);
+        //newPipe.transform.position = new Vector3(npoint.x, npoint.y, npoint.z);
+        
     }
 
     //Middle position
@@ -149,5 +201,53 @@ public class PipeManager : MonoBehaviour
 
         return possiblePos[Random.Range(0, possiblePos.Count)];
     }
+
+    private void MakeBulbPipe(Vector3 pos, Vector3Int dir)
+    {
+        if (dir.magnitude < 0.1) { return; }
+        GameObject newbulbPipe = Instantiate(
+            bulbPipe, pos, Quaternion.FromToRotation(Vector3Int.forward, dir), this.transform
+        );
+        newbulbPipe.transform.localPosition = pos;
+    }
+
+    private Vector3Int CheckNewPipeRequirements()
+    {
+            // Check if there are spots available in positions
+            bool spotsAvailable = false;
+            foreach (int val in cells)
+            {
+                if (val == 0)
+                {
+                    spotsAvailable = true;
+                    break;
+                }
+            }
+
+            // If we can't continue building a new pipe, return
+            if (!spotsAvailable)
+            {
+                return new Vector3Int(-1,-1,-1);
+            }
+
+            // Find a spot to begin the next pipe
+            int sIndex = Random.Range(0, cells.Length);
+            while (cells[sIndex] == 1)
+            {
+                sIndex++;
+                if (sIndex >= cells.Length)
+                {
+                    sIndex = 0;
+                }
+            }
+
+            // Calculate the new pipe position and return it
+            Vector3Int ret = Vector3Int.zero;
+            ret.z = sIndex / (lenX * lenY);
+            sIndex -= ret.z * lenX * lenY;
+            ret.y = sIndex / (lenX);
+            ret.x = sIndex - (ret.y * lenX);
+            return ret;
+        }
 }
 
